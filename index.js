bea1ab32b672954a589d07912475484beb0387e9
lucasmarinn <lucasmarin27@gmail.com>
2024-01-11 21:57:08 -0300

    codigo finalizado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Teste")
+function pontuacaoRankeadas(quantVitorias, quantDerrotas){
+    let saldo = quantVitorias - quantDerrotas
+    let resultado = "0"
+    if (saldo <= 10){
+        resultado = "Ferro"
+    } else if(saldo <= 20){
+        resultado = "Bronze"
+    } else if(saldo <= 50){
+        resultado = "Prata"
+    } else if(saldo <= 80){
+        resultado = "Ouro"
+    } else if(saldo <= 90){
+        resultado = "Diamante"
+    } else if(saldo <= 100){
+        resultado = "Lendário"
+    } else{
+        resultado = "Imortal"
+    }
+    return `O Herói tem de saldo de ${saldo} está no nível de ${resultado}`
+
+}
+console.log(pontuacaoRankeadas(100, 20))
